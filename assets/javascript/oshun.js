4877b6cd34667dd5d843130674ea82b9599aa2c8
Ashley <ashleyh5440@gmail.com>
2023-11-06 22:17:02 -0500

    added previous page button that links to another html page when on page1

--- assets/javascript/oshun.js
+++ assets/javascript/oshun.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const pages = document.querySelectorAll(".page");
     const nextPageBtn = document.getElementById("nextPageBtn");
+    const previousPageBtn = document.getElementById("previousPageBtn");
     let currentPageIndex = 0;
   
     function showPage(pageIndex) {
@@ -15,10 +16,21 @@ document.addEventListener("DOMContentLoaded", function() {
         const nextPageIndex = currentPageIndex + 1;
         showPage(nextPageIndex);
       } else {
-        // if on the last page, go to ne html file
-        window.location.href = "index.html"; 
+        // if on the last page, go to linked html file
+        window.location.href = "html-file-name.html"; 
       }
     });
+
+    previousPageBtn.addEventListener("click", function() {
+        if (currentPageIndex === 0) {
+          // if on page1, go to linked html file
+          window.location.href = "html-file-name.html";
+        } else {
+          //go to previous page
+          const previousPageIndex = currentPageIndex - 1;
+          showPage(previousPageIndex);
+        }
+      });
   
     // show initial page
     showPage(currentPageIndex);
