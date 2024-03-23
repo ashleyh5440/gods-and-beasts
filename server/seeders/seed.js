9c15fdf5c5750c4b2baa79eae0dd764625f64802
Ashley <ashleyh5440@gmail.com>
2024-03-23 08:35:28 -0400

    finished seed.js, added character seeder and auth util

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,2 +1,15 @@
 const db = require('../config/connection');
 const { Character, User, UserDeck } = require('../models');
+const characterSeeds = require('./characterSeeds.json');
+
+const cleanDB = require('./cleanDB');
+
+db.once('open', async () => {
+    try {
+        await cleanDB('Character', 'characters');
+        await cleanDB('User', 'users');
+        await cleanDB('UserDeck', 'userdecks');
+    } catch (error) {
+        console.error('Error in cleaning database', error);
+    }
+});
